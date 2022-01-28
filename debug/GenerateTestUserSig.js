92f7b12e7fbcfc21ca58a64f2a09e233f343720f
WinsunLove <874327531@qq.com>
2022-01-28 20:35:56 +0800

    数据脱敏

--- debug/GenerateTestUserSig.js
+++ debug/GenerateTestUserSig.js
@@ -6,7 +6,7 @@ import LibGenerateTestUserSig from './lib-generate-test-usersig-es.min.js'
  * 进入腾讯云实时音视频[控制台](https://console.cloud.tencent.com/rav ) 创建应用，即可看到 SDKAppId，
  * 它是腾讯云用于区分客户的唯一标识。
  */
-const SDKAPPID = 1400504130
+const SDKAPPID = 0
 
 
 /**
@@ -28,7 +28,7 @@ const EXPIRETIME = 604800
  * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
  * 文档：https://cloud.tencent.com/document/product/647/17275#Server
  */
-const SECRETKEY = '129bd80a7ce91d8dcadc71b626388c260edba41e9f5e526d5e000e785952797c'
+const SECRETKEY = ''
 
 /*
  * Module:   GenerateTestUserSig
